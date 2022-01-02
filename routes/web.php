67de1b589602695d51c75f236e351d6fe69cb9a4
Sherlok Homs <ivseldon@ya.ru>
2022-01-02 16:18:42 +0300

    feat: add PageController

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,5 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('about', function () {
-    return view('about');
-});
+Route::get('about', [PageController::class, 'about']);
+
